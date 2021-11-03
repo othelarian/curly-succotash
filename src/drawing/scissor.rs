729e156f22d820406767f62d3171cce56e95c19f
othelarian <le.maitre.killian@gmail.com>
2021-11-03 22:25:05 +0100

    removing "Paint" trait

--- src/drawing/scissor.rs
+++ src/drawing/scissor.rs
@@ -1,4 +1,4 @@
-use nvg::{Context, Point, Extent, Rect, Paint, Color};
+use nvg::{Context, Point, Extent, Rect, Color};
 use nvg_gl::Renderer;
 
 
@@ -12,19 +12,12 @@ pub fn draw(
     ctx.rotate((5 as f32).to_radians());
     ctx.save();
     ctx.begin_path();
-    ctx.rect(Rect::new(
-        Point::new(-20.0, -20.0),
-        Extent::new(60.0, 40.0)
-    ));
-    ctx.fill_paint(Paint::from(Color::rgba_i(255, 0, 0, 255)));
+    ctx.rect(Rect::new(Point::new(-20.0, -20.0), Extent::new(60.0, 40.0)));
+    ctx.fill_paint(Color::rgba_i(255, 0, 0, 255));
     ctx.fill().unwrap();
-    ctx.scissor(Rect::new(
-        Point::new(20.0, 20.0),
-        Extent::new(60.0,40.0))
-    );
+    ctx.scissor(Rect::new(Point::new(20.0, 20.0), Extent::new(60.0,40.0)));
 
     // Draw second rectangle with ofset and rotation.
-
     ctx.translate(40.0, 0.0);
     ctx.rotate(t);
 
@@ -32,25 +25,16 @@ pub fn draw(
     ctx.save();
     ctx.reset_scissor();
     ctx.begin_path();
-    ctx.rect(Rect::new(
-        Point::new(-20.0, -10.0),
-        Extent::new(60.0, 30.0)
-    ));
-    ctx.fill_paint(Paint::from(Color::rgba_i(255, 128, 0, 64)));
+    ctx.rect(Rect::new(Point::new(-20.0, -10.0), Extent::new(60.0, 30.0)));
+    ctx.fill_paint(Color::rgba_i(255, 128, 0, 64));
     ctx.fill().unwrap();
     ctx.restore();
 
     // Draw second rectangle with combined scissoring.
-    ctx.intersect_scissor(Rect::new(
-        Point::new(-20.0,-10.0),
-        Extent::new(60.0, 30.0)
-    ));
+    ctx.intersect_scissor(Rect::new(Point::new(-20.0,-10.0), Extent::new(60.0, 30.0)));
     ctx.begin_path();
-    ctx.rect(Rect::new(
-        Point::new(-20.0,-10.0),
-        Extent::new(60.0, 30.0)
-    ));
-    ctx.fill_paint(Paint::from(Color::rgba_i(255, 128, 0, 255)));
+    ctx.rect(Rect::new(Point::new(-20.0,-10.0), Extent::new(60.0, 30.0)));
+    ctx.fill_paint(Color::rgba_i(255, 128, 0, 255));
     ctx.fill().unwrap();
     ctx.restore();
 
